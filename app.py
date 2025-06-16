6783047cf3f6ae183ece061497d72fc6613f7571
MrJonson <flashh@list.ru>
2025-06-17 01:21:52 +0300

    start site project

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)          # Роут для главной страницы
